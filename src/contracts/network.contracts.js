85b0a0219372bd9b8549097b9664deb4bfd41a91
cameron <debliquy.cameron@gmail.com>
2022-11-03 14:10:55 +0100

    feat: update index html

--- src/contracts/network.contracts.js
+++ src/contracts/network.contracts.js
@@ -15,35 +15,51 @@ export const localhost = {
 }
 
 export const avalanche_fuji = {
-	chainId: '0xa869',
-	chainName: 'Avalanche Fuji',
-	nativeCurrency: {
-		name: 'AVAX',
-		symbol: 'AVAX',
-		decimals: 18,
-	},
-	rpcUrls: [
-		'https://api.avax-test.network/ext/bc/C/rpc'
-	],
-	blockExplorerUrls: [
-		'https://testnet.snowtrace.io/'
-	],
+    chainId: '0xa869',
+    chainName: 'Avalanche Fuji',
+    nativeCurrency: {
+        name: 'AVAX',
+        symbol: 'AVAX',
+        decimals: 18,
+    },
+    rpcUrls: [
+        'https://api.avax-test.network/ext/bc/C/rpc'
+    ],
+    blockExplorerUrls: [
+        'https://testnet.snowtrace.io/'
+    ],
 }
 
 export const binance = {
-	chainId: '0x38',
-	chainName: 'Binance Smart Chain',
-	nativeCurrency: {
-		name: 'BNB',
-		symbol: 'BNB',
-		decimals: 8,
-	},
-	rpcUrls: [
-		'https://bsc-dataseed.binance.org/'
-	],
-	blockExplorerUrls: [
-		'https://bscscan.com'
-	],
+    chainId: '0x38',
+    chainName: 'Binance Smart Chain',
+    nativeCurrency: {
+        name: 'BNB',
+        symbol: 'BNB',
+        decimals: 8,
+    },
+    rpcUrls: [
+        'https://bsc-dataseed.binance.org/'
+    ],
+    blockExplorerUrls: [
+        'https://bscscan.com'
+    ],
 }
 
-export default avalanche_fuji
+export const binance_test = {
+    chainId: '0x61',
+    chainName: 'Binance Smart Chain Testnet',
+    nativeCurrency: {
+        name: 'BNB',
+        symbol: 'BNB',
+        decimals: 18,
+    },
+    rpcUrls: [
+        'https://data-seed-prebsc-1-s1.binance.org:8545/'
+    ],
+    blockExplorerUrls: [
+        'https://testnet.bscscan.com/'
+    ],
+}
+
+export default binance_test
